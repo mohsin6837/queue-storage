f3bec300bdb265c803ba17150804d285d680ad99
Syed Mohsin <syedmohsin0720@gmail.com>
2025-08-05 06:05:01 +0530

    Update AzureQueueService.java

--- src/main/java/com/example/queue_storage/service/AzureQueueService.java
+++ src/main/java/com/example/queue_storage/service/AzureQueueService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.ManagedIdentityCredentialBuilder;
 import com.azure.storage.queue.QueueClient;
 import com.azure.storage.queue.QueueClientBuilder;
 
@@ -17,7 +18,7 @@ public class AzureQueueService {
             @Value("${azure.storage.account-name}") String accountName,
             @Value("${azure.storage.queue-name}") String queueName
     ) {
-        TokenCredential credential = new DefaultAzureCredentialBuilder().build();
+        TokenCredential credential = new ManagedIdentityCredentialBuilder().build();
 
         String endpoint = String.format("https://%s.queue.core.windows.net", accountName);
 
